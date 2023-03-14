1823e8fc61555ed66dfeddd91e6c6f8442f609ce
thibaud-heub <thibaud.heub@gmail.com>
2023-03-14 11:27:53 +0100

    Add a page to add users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const addRouter = require("./routes/add");
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/add", addRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/add.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+/* GET add. */
+router.get("/", function (req, res, next) {
+    res.render("add", { title: "Express" });
+});
+
+/* POST add. */
+router.post("/", function (req, res, next) {
+    const users = JSON.parse(fs.readFileSync("data/users.json"));
+    users.push(req.body);
+    users[users.length - 1].id = users[users.length - 2].id + 1;
+    users[users.length - 1].created_at = new Date();
+    fs.writeFileSync("data/users.json", JSON.stringify(users));
+    res.redirect("/");
+});
+
+module.exports = router;
