23d54dbd394af322fc0624f43070219fa1e10a55
thibaud-heub <thibaud.heub@gmail.com>
2023-03-14 10:54:16 +0100

    Add users list in index.html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
+    const users = JSON.parse(fs.readFileSync("data/users.json"));
+    res.render("index", { title: "Express", users });
 });
 
 module.exports = router;
